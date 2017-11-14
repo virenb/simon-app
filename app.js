127a4d58fe18500b302b012e9c058658dd49a26b
Viren Bhagat <virenb@gmail.com>
2017-11-13 23:19:07 -0500

    Fixed logic, added reset button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Randomize button color
 // Each button has a sound associated with it
+// Randomize button color
 // Sound when you press a button
 // Step counter
 // Restart the game and the counter
@@ -12,13 +12,14 @@ const blue = document.getElementById('blue').id;
 const yellow = document.getElementById('yellow').id;
 const buttons = [green, red, blue, yellow];
 let color;
-let counter = 0;
+let counter = document.getElementById('counter').innerHTML;
 let moves = [];
 let nextMove;
+document.getElementById('resetButton').style.display = 'none';
 
 let selectColor = (event) => {
   color = event.target.id;
-  console.log(color);
+  playGame();
 };
 
 document.getElementById(green).addEventListener('click', selectColor, false);
@@ -27,7 +28,38 @@ document.getElementById(blue).addEventListener('click', selectColor, false);
 document.getElementById(yellow).addEventListener('click', selectColor, false);
 
 let startGame = () => {
-nextMove = buttons[Math.floor(Math.random() * buttons.length)];
-moves.push(nextMove);
-console.log(moves);
+  document.getElementById('startButton').style.display = 'none';
+  document.getElementById('resetButton').style.display = '';
+  console.log(counter);
+  nextMove = buttons[Math.floor(Math.random() * buttons.length)];
+  moves.push(nextMove);
+  console.log('This is the starting off ' + moves);
+  console.log('Moves: ' + moves + 'Color: ' + color);
+
+};
+
+let playGame = () => {
+  if (color === moves[counter]) {
+      nextMove = buttons[Math.floor(Math.random() * buttons.length)];
+      moves.push(nextMove);
+      console.log(moves);
+      counter++;
+      document.getElementById('counter').innerHTML++;
+    document.getElementById('message').innerHTML = "";
+
+    }
+  else {
+    color = "";
+    document.getElementById('message').innerHTML = "Wrong, try again!"
+    console.log('wrong');
+  }
+}
+
+let resetGame = () => {
+  moves = [];
+  counter = 0;
+  document.getElementById('counter').innerHTML = 0;
+  document.getElementById('message').innerHTML = '';
+  document.getElementById('startButton').style.display = '';
+  document.getElementById('resetButton').style.display = 'none';
 };
